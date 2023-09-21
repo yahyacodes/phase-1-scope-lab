35776b99fda2f8319947ab0a4ced2deebb9e6a0a
yahyacodes <yahya.codin@gmail.com>
2023-09-21 18:06:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new least favorite customer";
+}
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = "new least favorite customer";
+  } catch (error) {
+    throw error;
+  }
+}
+function changeLeastFavoriteCustomer() {
+  throw new Error("Assignment to constant variable.");
+}
